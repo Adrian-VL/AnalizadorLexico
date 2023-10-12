d9f886de94abaf42f0be64e7b8b76fade1b8bb5a
LuisMarin321 <104316339+LuisMarin321@users.noreply.github.com>
2023-10-11 21:15:02 -0600

    Update scanner.java

--- scanner.java
+++ scanner.java
@@ -71,55 +71,67 @@ public class Scanner {
                         estado = 16;
                         lexema += c;
                     }
+                    // Verifica si el carácter es una barra diagonal (slash)
                     else if(c == '/'){
-                        estado = 17;
-                        lexema += c;
-                    }
-                    else if(c == '+'){
-                        estado = 21;
-                        lexema += c;
-                    }
-                    else if(c == '-'){
-                        estado = 22;
-                        lexema += c;
-                    }
-                    else if(c == '*'){
-                        estado = 23;
-                        lexema += c;
-                    }
-                    else if(c == '{'){
-                        estado = 24;
-                        lexema += c;
-                    }
-                    else if(c == '}'){
-                        estado = 25;
-                        lexema += c;
-                    }
-                    else if(c == '('){
-                        estado = 26;
-                        lexema += c;
-                    }
-                    else if(c == ')'){
-                        estado = 27;
-                        lexema += c;
-                    }
-                    else if(c == ','){
-                        estado = 28;
-                        lexema += c;
-                    }
-                    else if(c == '.'){
-                        estado = 29;
-                        lexema += c;
-                    }
-                    else if(c == ';'){
-                        estado = 30;
-                        lexema += c;
-                    }
-                    else if(!Character.isLetterOrDigit(c) && c!='\n' && c!=' ' && c!='\t' && c!='\r'){
-                        estado = 31;
-                        i--;
-                        lexema = "";
-                    }
+                      estado = 17;           // Establece el estado a 17
+                      lexema += c;           // Añade el carácter al lexema
+                   }
+                   // Verifica si el carácter es un signo más
+                   else if(c == '+'){
+                     estado = 21;           // Establece el estado a 21
+                     lexema += c;           // Añade el carácter al lexema
+                   }
+                   // Verifica si el carácter es un signo menos
+                   else if(c == '-'){
+                     estado = 22;           // Establece el estado a 22
+                     lexema += c;           // Añade el carácter al lexema
+                   }
+                   // Verifica si el carácter es un asterisco
+                   else if(c == '*'){
+                     estado = 23;           // Establece el estado a 23
+                     lexema += c;           // Añade el carácter al lexema
+                   }
+                   // Verifica si el carácter es una llave de apertura
+                  else if(c == '{'){
+                     estado = 24;           // Establece el estado a 24
+                     lexema += c;           // Añade el carácter al lexema
+                   }
+                   // Verifica si el carácter es una llave de cierre
+                  else if(c == '}'){
+                     estado = 25;           // Establece el estado a 25
+                     lexema += c;           // Añade el carácter al lexema
+                   }
+                   // Verifica si el carácter es un paréntesis de apertura
+                 else if(c == '('){
+                    estado = 26;           // Establece el estado a 26
+                    lexema += c;           // Añade el carácter al lexema
+                   }
+                   // Verifica si el carácter es un paréntesis de cierre
+                 else if(c == ')'){
+                    estado = 27;           // Establece el estado a 27
+                    lexema += c;           // Añade el carácter al lexema
+                   }
+                   // Verifica si el carácter es una coma
+                 else if(c == ','){
+                    estado = 28;           // Establece el estado a 28
+                    lexema += c;           // Añade el carácter al lexema
+                   }
+                   // Verifica si el carácter es un punto
+                 else if(c == '.'){
+                    estado = 29;           // Establece el estado a 29
+                    lexema += c;           // Añade el carácter al lexema
+                   }
+                   // Verifica si el carácter es un punto y coma
+                 else if(c == ';'){
+                    estado = 30;           // Establece el estado a 30
+                    lexema += c;           // Añade el carácter al lexema
+                   }
+                   // Verifica si el carácter no es una letra o un dígito y no es ninguno de los caracteres listados
+                 else if(!Character.isLetterOrDigit(c) && c!='\n' && c!=' ' && c!='\t' && c!='\r'){
+                    estado = 31;           // Establece el estado a 31
+                    i--;                   // Decrementa el valor de i
+                    lexema = "";           // Limpia el valor del lexema
+                   }
               break;
                case 1:
                     if(c == '='){
@@ -320,131 +332,158 @@ public class Scanner {
                         lexema = "";
                     }
                 break;
+// Cuando el estado es 17
                 case 17:
                     if(c == '/'){
-                        estado = 20;
-                        lexema += c;
+                        estado = 20;           // Cambio al estado 20 si se encuentra otra barra diagonal (comentario de línea)
+                        lexema += c;           // Añade el carácter al lexema
                     }
                     else if(c == '*'){
-                        estado = 18;
-                        lexema = "";
+                        estado = 18;           // Cambio al estado 18 (inicio de comentario de bloque)
+                        lexema = "";           // Limpia el lexema
                     }
                     else{
-                        Token t = new Token(TipoToken.SLASH, lexema);
+                        Token t = new Token(TipoToken.SLASH, lexema); // Crea un token para una sola barra diagonal
                         tokens.add(t);
                         i--;
                         lexema = "";
                         estado = 0;
                     }
-                break;
+                break;          
+                // Tratando comentario de bloque
                 case 18:
                     if(c != '*'){
-                        estado = 18;
+                        estado = 18;           // Permanece en el estado 18 si no encuentra '*'
                         if(i == (source.length() - 1)){
-                            Interprete.error(linea, "Se esperaba '*/'");
+                            Interprete.error(linea, "Se esperaba '*/'"); // Error si no se cierra el comentario
                         }
                     }
                     else{
-                        estado = 19;
+                        estado = 19;           // Cambio al estado 19 si se encuentra '*'
                     }
                 break;
+                
+                // Estado intermedio en comentario de bloque
                 case 19:
                     if(c == '*'){
-                        estado = 19;
+                        estado = 19;           // Permanece en el estado 19 si encuentra otro '*'
                     }
                     else if(c == '/'){
-                        estado = 0;
+                        estado = 0;            // Final del comentario de bloque
                     }
                     else{
-                        estado = 18;
+                        estado = 18;           // Regresa al estado 18 si no encuentra '/'
                         if(i == (source.length() - 1)){
-                            Interprete.error(linea, "Se esperaba '*/'");
+                            Interprete.error(linea, "Se esperaba '*/'"); // Error si no se cierra el comentario
                         }
                     }
                 break;
+                
+                // Tratando comentario de línea
                 case 20:
                     if(c != '\n'){
-                        estado = 20;
-                        lexema += c;
+                        estado = 20;           // Permanece en el estado 20 mientras no encuentre un salto de línea
+                        lexema += c;           // Añade el carácter al lexema
                     }
                     else{
-                        estado = 0;
+                        estado = 0;            // Final del comentario de línea
                         lexema = "";
                     }
                 break;
+                               // Caso para el signo más
                 case 21:{
-                    Token t = new Token(TipoToken.PLUS, lexema);
+                    Token t = new Token(TipoToken.PLUS, lexema);   // Crea un token para el signo más
+                    tokens.add(t);                                 // Añade el token a la lista de tokens
+                    i--;                                           // Decrementa el índice
+                    lexema = "";                                   // Limpia el lexema
+                    estado = 0;                                    // Reinicia el estado
+                break;}
+                
+                // Caso para el signo menos
+                case 22:{
+                    Token t = new Token(TipoToken.MINUS, lexema);  // Crea un token para el signo menos
+                    tokens.add(t);                                 // ...
+                    i--;                                           // ...
+                    lexema = "";                                   // ...
+                    estado = 0;                                    // ...
+                break;}
+
+                // Caso para el asterisco
+                case 23:{
+                    Token t = new Token(TipoToken.STAR, lexema);   // Crea un token para el asterisco
                     tokens.add(t);
                     i--;
                     lexema = "";
                     estado = 0;
                 break;}
-                case 22:{
-                        Token t = new Token(TipoToken.MINUS, lexema);
-                        tokens.add(t);
-                        i--;
-                        lexema = "";
-                        estado = 0;
-                break;}
-                case 23:{
-                        Token t = new Token(TipoToken.STAR, lexema);
-                        tokens.add(t);
-                        i--;
-                        lexema = "";
-                        estado = 0;
-                break;}
+
+                // Caso para la llave de apertura
                 case 24:{
-                        Token t = new Token(TipoToken.LEFT_BRACE, lexema);
-                        tokens.add(t);
-                        i--;
-                        lexema = "";
-                        estado = 0;
-                break;}
-                case 25:{
-                        Token t = new Token(TipoToken.RIGHT_BRACE, lexema);
-                        tokens.add(t);
-                        i--;
-                        lexema = "";
-                        estado = 0;
-                break;}
-                case 26:{
-                        Token t = new Token(TipoToken.LEFT_PAREN, lexema);
-                        tokens.add(t);
-                        i--;
-                        lexema = "";
-                        estado = 0;
-                break;}
-                case 27:{
-                        Token t = new Token(TipoToken.RIGHT_PAREN, lexema);
-                        tokens.add(t);
-                        i--;
-                        lexema = "";
-                        estado = 0;
-                break;}
-                case 28:{
-                        Token t = new Token(TipoToken.COMMA, lexema);
-                        tokens.add(t);
-                        i--;
-                        lexema = "";
-                        estado = 0;
-                break;}
-                case 29:{
-                        Token t = new Token(TipoToken.DOT, lexema);
-                        tokens.add(t);
-                        i--;
-                        lexema = "";
-                        estado = 0;
-                break;}
-                case 30:{
-                        Token t = new Token(TipoToken.SEMICOLON, lexema);
-                        tokens.add(t);
-                        i--;
-                        lexema = "";
-                        estado = 0;
-                break;}
-                case 31:
-                    Interprete.error(linea, "Expresion no valida");
+                    Token t = new Token(TipoToken.LEFT_BRACE, lexema); // Crea un token para '{'
+                    tokens.add(t);
+                    i--;
+                    lexema = "";
                     estado = 0;
+                break;}
+
+                // Caso para la llave de cierre
+                case 25:{
+                    Token t = new Token(TipoToken.RIGHT_BRACE, lexema); // Crea un token para '}'
+                    tokens.add(t);
+                    i--;
+                    lexema = "";
+                    estado = 0;
+                break;}
+
+                // Caso para el paréntesis de apertura
+                case 26:{
+                    Token t = new Token(TipoToken.LEFT_PAREN, lexema);  // Crea un token para '('
+                    tokens.add(t);
+                    i--;
+                    lexema = "";
+                    estado = 0;
+                break;}
+
+                // Caso para el paréntesis de cierre
+                case 27:{
+                    Token t = new Token(TipoToken.RIGHT_PAREN, lexema); // Crea un token para ')'
+                    tokens.add(t);
+                    i--;
+                    lexema = "";
+                    estado = 0;
+                break;}
+
+                // Caso para la coma
+                case 28:{
+                    Token t = new Token(TipoToken.COMMA, lexema);       // Crea un token para la coma
+                    tokens.add(t);
+                    i--;
+                    lexema = "";
+                    estado = 0;
+                break;}
+
+                // Caso para el punto
+                case 29:{
+                    Token t = new Token(TipoToken.DOT, lexema);         // Crea un token para el punto
+                    tokens.add(t);
+                    i--;
+                    lexema = "";
+                    estado = 0;
+                break;}
+
+                // Caso para el punto y coma
+                case 30:{
+                    Token t = new Token(TipoToken.SEMICOLON, lexema);   // Crea un token para el punto y coma
+                    tokens.add(t);
+                    i--;
+                    lexema = "";
+                    estado = 0;
+                break;}
+
+                // En caso de expresión no válida
+                case 31:
+                    Interprete.error(linea, "Expresion no valida");     // Muestra un mensaje de error para una expresión no válida
+                    estado = 0;                                          // Reinicia el estado
                 break;
             }
         }
